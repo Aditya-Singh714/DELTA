02b60af6700bca6cf8dad652d44439548750ef8e
Aditya Singh <83707197+Aditya-Singh714@users.noreply.github.com>
2023-08-02 11:26:42 +0530

    Add files via upload

---
+++ JavaScript Part 10/todo.js
@@ -0,0 +1,31 @@
+let btn = document.querySelector('button');
+let ul = document.querySelector('ul');
+let inp = document.querySelector('input');
+
+btn.addEventListener("click", function(){
+    let item = document.createElement('li');
+    item.innerText = inp.value;
+
+    let delBtn = document.createElement('button');
+    delBtn.innerText = "Delete";
+    delBtn.classList.add("delete");
+
+    item.appendChild(delBtn);
+    ul.appendChild(item);
+    inp.value = "";
+});
+
+ul.addEventListener("click", function(event){
+    if(event.target.nodeName == "BUTTON"){
+        let listItem = event.target.parentElement;
+        listItem.remove();
+    }
+})
+
+// let delBtns = document.querySelectorAll(".delete");
+// for(delBtn of delBtns){
+//     delBtn.addEventListener("click", function(){
+//         let par = this.parentElement;
+//         par.remove();
+//     });
+// }
