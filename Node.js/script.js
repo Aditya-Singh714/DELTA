fdae0bbb716e13386f827089155a50152b399c2d
Aditya Singh <83707197+Aditya-Singh714@users.noreply.github.com>
2023-08-22 12:50:32 +0530

    Create script.js

---
+++ Node.js/script.js
@@ -0,0 +1,37 @@
+// import {add, mul, pow} from "./math.js";
+
+// import { generate } from "random-words";
+
+// const maths = require("./math")
+
+// const info = require("./Fruits");
+
+// let n = 10;
+// for( let i = 0; i < n; i++){
+//     console.log("Hello Node ", i);
+// }
+
+// console.log(process.argv);
+
+// console.log(maths);
+// console.log(maths.add(6,42));
+// console.log(maths.mul(23,11));
+// console.log(maths.sub(69,59));
+// console.log(maths.pow(5,5));
+
+// console.log(info);
+// console.log(info[0]);
+// console.log(info[1].name);
+
+// console.log(pow(6,6));
+// console.log(mul(54,6));
+
+// console.log(generate());
+// console.log(generate());
+// console.log(generate());
+// console.log(generate());
+
+let name = "Professor";
+console.log(name);
+let age = 23;
+console.log(age);
