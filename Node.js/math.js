ca4acc491c5ef8e9e6ed23000a5b9d2e5f6a04db
Aditya Singh <83707197+Aditya-Singh714@users.noreply.github.com>
2023-08-22 12:51:54 +0530

    Add files via upload

---
+++ Node.js/math.js
@@ -0,0 +1,33 @@
+export const add = (a, b) => a+b;
+export const mul = (a, b) => a*b;
+export const sub = (a, b) => a-b;
+export const pow = (a, b) => a**b;
+export const g = 9.8;
+export const PI = 3.14;
+
+// module.exports.add = (a, b) => a+b;
+// module.exports.mul = (a, b) => a*b;
+// module.exports.sub = (a, b) => a-b;
+// module.exports.pow = (a, b) => a**b;
+// module.exports.g = 9.8;
+// module.exports.PI = 3.14;
+
+// let obj = {
+//     add : add,
+//     mul : mul,
+//     sub : sub,
+//     pow : pow,
+//     g : g,
+//     pi : PI
+// };
+
+// module.exports = {
+//     add : add,
+//     mul : mul,
+//     sub : sub,
+//     pow : pow,
+//     g : g,
+//     pi : PI
+// };
+
+// module.exports = obj;
