286d1409ad508f96f9325c3d073cb0f92d1d7ccd
Aditya Singh <83707197+Aditya-Singh714@users.noreply.github.com>
2023-10-30 23:42:04 +0530

    Create App.jsx

---
+++ React-lec1/basic-react-app/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Title from './Title.jsx'
+import ProductTab from './ProductTab';
+
+function App() {
+  return (
+    <>
+      <Title />
+      <ProductTab />
+    </>
+  );
+}
+
+export default App
